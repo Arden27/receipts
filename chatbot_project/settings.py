bef247aafad48fb19ef80d792b463c237175b333
Artem Furman <artemfurman@gmail.com>
2023-06-26 00:15:16 +0200

    media removed from settings

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -152,8 +152,8 @@ STATICFILES_DIRS = [
     FRONTEND_DIR / "react/receipts-frontend/build/static",
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
