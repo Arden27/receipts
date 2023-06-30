cb5cd15d95736e5e1f1f668ea84728bdb3b542c9
Artem Furman <artemfurman@gmail.com>
2023-06-30 16:34:18 +0200

    max_digits for prices raised from 10 to 20

--- receipts/models.py
+++ receipts/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Receipt(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     date = models.DateField()
     store = models.CharField(max_length=200)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=20, decimal_places=2)
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 class ReceiptItem(models.Model):
     item_name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     receipt = models.ForeignKey(Receipt, on_delete=models.CASCADE, null=True)
 
