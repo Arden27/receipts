c5274390645c818dd13e382e12f07528f63e9123
Brydak <brydak@outlook.com>
2023-07-24 20:46:21 +0200

    .

--- frontend/react/receipts-frontend/src/components/MainApp.js
+++ frontend/react/receipts-frontend/src/components/MainApp.js
@@ -1,83 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import ReceiptForm from './ReceiptForm';
-import ReceiptList from './ReceiptList';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ReceiptForm from "./ReceiptForm";
+import ReceiptList from "./ReceiptList";
 //import ReceiptItemList from './ReceiptItemList';
 import { useNavigate } from "react-router-dom";
-import CategoryList from './CategoryList';
-import Totals from './Totals';
-import { resetStore, setAuthError } from '../redux/store';
-import { logoutUser } from '../api';
+import CategoryList from "./CategoryList";
+import Totals from "./Totals";
+import { resetStore, setAuthError } from "../redux/store";
+import { logoutUser } from "../api";
 
 function MainApp({ setIsLoggedIn }) {
-    const [addingReceipt, setAddingReceipt] = useState(false);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+	const [addingReceipt, setAddingReceipt] = useState(false);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-    const isAuthError = useSelector(state => state.isAuthError);
+	const isAuthError = useSelector((state) => state.isAuthError);
 
-    useEffect(() => {
-        dispatch({ type: 'SET_SHOULD_REFRESH', payload: true });
-    }, [dispatch]);
-    
-    useEffect(() => {
-        if (isAuthError) {
-            setIsLoggedIn(false);
-            navigate('/login');
-            dispatch(setAuthError(false));
-        }
-    }, [isAuthError, setIsLoggedIn, navigate, dispatch]);
+	useEffect(() => {
+		dispatch({ type: "SET_SHOULD_REFRESH", payload: true });
+	}, [dispatch]);
 
-    const handleToggleReceiptForm = () => {
-        setAddingReceipt(!addingReceipt);
-    };
+	useEffect(() => {
+		if (isAuthError) {
+			setIsLoggedIn(false);
+			navigate("/login");
+			dispatch(setAuthError(false));
+		}
+	}, [isAuthError, setIsLoggedIn, navigate, dispatch]);
 
-    const handleLogout = async () => {
-        try {
-            await logoutUser();
-        } catch (error) {
-            console.log(error);
-        } finally {
-            localStorage.removeItem('token');
-            dispatch(resetStore());
-            setIsLoggedIn(false);
-            navigate('/login');
-        }
-    };
+	const handleToggleReceiptForm = () => {
+		setAddingReceipt(!addingReceipt);
+	};
 
-    const handleReceiptSubmit = () => {
-        setAddingReceipt(false);
-        dispatch({ type: 'SET_SHOULD_REFRESH', payload: true });
-    };
+	const handleLogout = async () => {
+		try {
+			await logoutUser();
+		} catch (error) {
+			console.log(error);
+		} finally {
+			localStorage.removeItem("token");
+			dispatch(resetStore());
+			setIsLoggedIn(false);
+			navigate("/login");
+		}
+	};
 
-    return (
-        <div className="App grid grid-cols-2 grid-rows-auto-1fr-1fr h-screen gap-4">
-            <nav className="row-span-1 col-span-2 flex justify-between bg-blue-400 p-4 text-white">
-                <button 
-                    onClick={handleToggleReceiptForm} 
-                    className={addingReceipt ? "font-bold py-2 px-4 rounded bg-white text-red-500 border border-red-500 hover:bg-red-500 hover:text-white" : "font-bold py-2 px-4 rounded bg-green-500 hover:bg-green-700"}
-                >
-                    {addingReceipt ? "Cancel" : "Add Receipt"}
-                </button>
-                <button 
-                    onClick={handleLogout} 
-                    className="font-bold py-2 px-4 rounded bg-red-500 hover:bg-red-700"
-                >
-                    Logout
-                </button>
-            </nav>
-            <div className="row-span-2 col-span-1 overflow-auto">
-                {addingReceipt && <ReceiptForm onSubmit={handleReceiptSubmit} />}
-                <ReceiptList />
-            </div>
-            <div className="row-start-2 col-start-2 overflow-auto">
-                <Totals />
-            </div>
-            <div className="row-start-3 col-start-2 overflow-auto">
-                <CategoryList />
-            </div>
-        </div>
-    );
+	const handleReceiptSubmit = () => {
+		setAddingReceipt(false);
+		dispatch({ type: "SET_SHOULD_REFRESH", payload: true });
+	};
+
+	return (
+		<div className="App grid h-screen  grid-cols-2 grid-rows-[auto_auto_1fr] gap-4">
+			<nav className="col-span-2 flex justify-between bg-blue-400 p-4 text-white">
+				<button
+					onClick={handleToggleReceiptForm}
+					className={
+						addingReceipt
+							? "rounded border border-red-500 bg-white px-4 py-2 font-bold text-red-500 hover:bg-red-500 hover:text-white"
+							: "rounded bg-green-500 px-4 py-2 font-bold hover:bg-green-700"
+					}
+				>
+					{addingReceipt ? "Cancel" : "Add Receipt"}
+				</button>
+				<button
+					onClick={handleLogout}
+					className="rounded bg-red-500 px-4 py-2 font-bold hover:bg-red-700"
+				>
+					Logout
+				</button>
+			</nav>
+			<div className="row-span-2">
+				{addingReceipt && (
+					<ReceiptForm onSubmit={handleReceiptSubmit} />
+				)}
+				<ReceiptList />
+			</div>
+			<div className="">
+				<Totals />
+			</div>
+			<div className="overflow-auto">
+				<CategoryList />
+			</div>
+		</div>
+	);
 }
 
 export default MainApp;
